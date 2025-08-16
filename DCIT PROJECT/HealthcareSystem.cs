d5bcff1f390135205c68797571891b25ee201077
Jaysab Concepts <Jaysab Concepts@DESKTOP-CNCE67M>
2025-08-16 01:16:07 +0000

    I added a healthcare system

---
+++ DCIT PROJECT/HealthcareSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HealthcareSystem
+{
+    // a. Generic Repository
+    public class Repository<T>
+    {
+        private List<T> items = new();
+
+        public void Add(T item) => items.Add(item);
+
+        public List<T> GetAll() => new List<T>(items);
+
+        public T? GetById(Func<T, bool> predicate) =>
+            items.FirstOrDefault(predicate);
+
+        public bool Remove(Func<T, bool> predicate)
+        {
+            var item = items.FirstOrDefault(predicate);
+            if (item != null)
+            {
+                items.Remove(item);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    // b. Patient class
+    public class Patient
+    {
+        p
